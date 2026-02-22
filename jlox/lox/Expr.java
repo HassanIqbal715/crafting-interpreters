9b4fcb275d859a4963414870e6a30c2f10d659d1
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2026-02-23 00:54:19 +0500

    Added logical and postfix expressions

--- jlox/lox/Expr.java
+++ jlox/lox/Expr.java
@@ -4,12 +4,30 @@ import java.util.List;
 
 abstract class Expr {
 	interface Visitor<R> {
+		R visitAssignExpr(Assign expr);
 		R visitBinaryExpr(Binary expr);
 		R visitGroupingExpr(Grouping expr);
 		R visitLiteralExpr(Literal expr);
+		R visitLogicalExpr(Logical expr);
 		R visitUnaryExpr(Unary expr);
+		R visitPostfixExpr(Postfix expr);
 		R visitVariableExpr(Variable expr);
 		R visitTernaryExpr(Ternary expr);
+	}
+
+	static class Assign extends Expr {
+		Assign(Token name, Expr value) {
+			this.name = name;
+			this.value = value;
+		}
+
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitAssignExpr(this);
+		}
+
+		final Token name;
+		final Expr value;
 	}
 
 	static class Binary extends Expr {
@@ -55,6 +73,23 @@ abstract class Expr {
 		final Object value;
 	}
 
+	static class Logical extends Expr {
+		Logical(Expr left, Token operator, Expr right) {
+			this.left = left;
+			this.operator = operator;
+			this.right = right;
+		}
+
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitLogicalExpr(this);
+		}
+
+		final Expr left;
+		final Token operator;
+		final Expr right;
+	}
+
 	static class Unary extends Expr {
 		Unary(Token operator, Expr right) {
 			this.operator = operator;
@@ -68,6 +103,21 @@ abstract class Expr {
 
 		final Token operator;
 		final Expr right;
+	}
+
+	static class Postfix extends Expr {
+		Postfix(Expr left, Token operator) {
+			this.left = left;
+			this.operator = operator;
+		}
+
+		@Override
+		<R> R accept(Visitor<R> visitor) {
+			return visitor.visitPostfixExpr(this);
+		}
+
+		final Expr left;
+		final Token operator;
 	}
 
 	static class Variable extends Expr {
