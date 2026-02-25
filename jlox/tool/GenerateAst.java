5ddc4ef6e933be279ba91abd865ca29ae2a6ae53
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2026-02-25 21:21:41 +0500

    Add array support

--- jlox/tool/GenerateAst.java
+++ jlox/tool/GenerateAst.java
@@ -13,25 +13,30 @@ public class GenerateAst {
         }
         String outputDir = args[0];
         defineAst(outputDir, "Expr", Arrays.asList(
-            "Assign   : Token name, Expr value",
-            "Binary   : Expr left, Token operator, Expr right",
-            "Grouping : Expr expression",
-            "Literal  : Object value",
-            "Logical  : Expr left, Token operator, Expr right",
-            "Unary    : Token operator, Expr right",
-            "Postfix  : Expr left, Token operator",
-            "Variable : Token name",
-            "Ternary  : Expr left, Token operator1, Expr mid, Token operator2, Expr right"
+            "Array      : Token name, Expr index",
+            "Assign     : Token name, Expr value",
+            "AssignArray: Token name, Expr index, Expr value",
+            "Binary     : Expr left, Token operator, Expr right",
+            "Call       : Expr callee, Token paren, List<Expr> arguments",
+            "Grouping   : Expr expression",
+            // "Elements : List<Expr> elements",
+            "Literal    : Object value",
+            "Logical    : Expr left, Token operator, Expr right",
+            "Unary      : Token operator, Expr right",
+            "Postfix    : Expr left, Token operator",
+            "Variable   : Token name",
+            "Ternary    : Expr left, Token operator1, Expr mid, Token operator2, Expr right"
         ));
 
         defineAst(outputDir, "Stmt", Arrays.asList(
+            "Arr : Token name, Expr size, List<Expr> elements",
             "Block : List<Stmt> statements",
             "Expression : Expr expression",
             "If : Expr condition, Stmt thenBranch, Stmt elseBranch",
             "Print : Expr expression",
             "Var : Token name, Expr initializer",
             "While : Expr condition, Stmt body",
-            "CommaDeclaration : List<Stmt.Var> declarations"
+            "CommaDeclaration : List<Stmt> declarations"
         ));
     }
 
