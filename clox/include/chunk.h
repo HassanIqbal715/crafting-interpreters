f8e38548580046d8b1ab5700ab4894ad8f2d2ca3
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2026-04-01 15:12:07 +0500

    Add Constant Long instruction to store 24bit long operands

--- clox/include/chunk.h
+++ clox/include/chunk.h
@@ -8,6 +8,7 @@
 // Enum with all the OpCodes
 typedef enum {
     OP_CONSTANT,
+    OP_CONSTANT_LONG,
     OP_RETURN
 } OpCode;
 
@@ -25,6 +26,7 @@ void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
 void writeChunk(Chunk* chunk, uint8_t byte, int line);
 int addConstant(Chunk* chunk, Value value);
+void writeConstant(Chunk* chunk, Value value, int line);
 int getLine(Chunk* chunk, int byteIndex);
 
 #endif
--- clox/src/chunk.c
+++ clox/src/chunk.c
@@ -40,6 +40,20 @@ int addConstant(Chunk* chunk, Value value) {
     return chunk->constants.count - 1; // return index to help locate it later.
 }
 
+void writeConstant(Chunk* chunk, Value value, int line) {
+    int constant = addConstant(chunk, value);
+    if (constant <= 255) {
+        writeChunk(chunk, OP_CONSTANT, line);
+        writeChunk(chunk, constant, line);
+    }
+    else {
+        writeChunk(chunk, OP_CONSTANT_LONG, line);
+        writeChunk(chunk, constant >> 16, line);
+        writeChunk(chunk, constant >> 8, line);
+        writeChunk(chunk, constant, line);
+    }
+}
+
 int getLine(Chunk* chunk, int byteIndex) {
     if (byteIndex >= chunk->count || chunk->lines.count <= 0) return -1;
 
