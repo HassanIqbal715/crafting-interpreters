28008bd0d01331f3408e6dfc3c9045031faadd1f
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2026-04-10 17:17:57 +0500

    Replace custom memory manager with C standard allocator

--- clox/include/memory_manager.h
+++ clox/include/memory_manager.h
@@ -6,53 +6,17 @@
 
 #include "common.h"
 
-typedef struct FreeHeader {
-    size_t size; // size of this free block
-    void* address; // starting address of this free block
-    struct FreeHeader* next; // next free block
-} FreeHeader;
-
-typedef struct {
-    void* memory;
-    size_t size;
-} Memory;
-
-typedef struct {
-    size_t size;
-} BlockMetaData;
-
-void initMemory(Memory* memory);
-void writeMemory(Memory* memory, size_t size);
-void freeMemory(Memory* memory);
-void* assignMem(Memory* memory, FreeHeader** head, size_t size);
-void* reassignMem(Memory* memory, FreeHeader** head, void* pointer, 
-    size_t oldSize, size_t newSize);
-void freeMem(Memory* memory, FreeHeader** head, void* address);
-
-void initFreeList(Memory* memory, FreeHeader** head);
-
-// starting size = 16, and grows by a factor 1.5 for every capacity increase
+// starting size = 16, and grows by a factor 2 for every capacity increase
 #define GROW_CAPACITY(capacity) \
-((capacity) < 16 ? 16 : (int)(capacity * 1.5))
+((capacity) < 8 ? 8 : (int)(capacity * 2))
 
 #define GROW_ARRAY(type, pointer, oldCount, newCount) \
 (type*)reallocate(pointer, sizeof(type) * (oldCount), \
-sizeof(type) * (newCount), 0)
+sizeof(type) * (newCount))
 
 #define FREE_ARRAY(type, pointer, oldCount) \
-reallocate(pointer, sizeof(type) * (oldCount), 0, false)
+reallocate(pointer, sizeof(type) * (oldCount), 0)
 
-#define MALLOC(size) \
-reallocate(NULL, 0, size, false)
-
-#define FREE(pointer) \
-reallocate(pointer, 0, 0, false)
-
-#define DESTROY_MEMORY() \
-reallocate(NULL, 0, 0, true)
-
-#define MEM_SIZE 1024 * sizeof(uint8_t) * 128
-
-void* reallocate(void* pointer, size_t oldSize, size_t newSize, bool destroy);
+void* reallocate(void* pointer, size_t oldSize, size_t newSize);
 
 #endif
