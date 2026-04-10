99108589472ca005287f4b179d2bf2cc543772e8
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2026-04-10 18:55:29 +0500

    Complete Scanner

--- clox/src/main.c
+++ clox/src/main.c
@@ -1,30 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
 #include "vm.h"
 
+// type code in command line
+static void repl() {
+    char line[1024];
+    for (;;) {
+        printf("> ");
+
+        if (!fgets(line, sizeof(line), stdin)) { // finish execution
+            printf("\n");
+            break;
+        }
+
+        interpret(line);
+    }
+}
+
+static char* readFile(const char* path) {
+    FILE* file = fopen(path, "rb");
+    if (file == NULL) {
+        fprintf(stderr, "Could not open file \"%s\".\n", path);
+        exit(74);
+    }
+
+    fseek(file, 0L, SEEK_END);
+    size_t fileSize = ftell(file);
+    rewind(file);
+
+    char* buffer = (char*)malloc(fileSize + 1);
+    if (buffer == NULL) {
+        fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
+        exit(74);
+    }
+
+    size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
+    if (bytesRead < fileSize) {
+        fprintf(stderr, "Could not read file \"%s\".\n", path);
+        exit(74);
+    }
+
+    buffer[bytesRead] = '\0';
+
+    fclose(file);
+    return buffer;
+}
+
+static void runFile(const char* path) {
+    char* source = readFile(path);
+    InterpretResult result = interpret(source);
+    for (char* c = &source[0]; *c != '\0'; c++) {
+        printf("%c", *c);
+    }
+    free(source);
+
+    if (result == INTERPRET_COMPILE_ERROR) exit(65);
+    if (result == INTERPRET_RUNTIME_ERROR) exit(70);
+}
+
 int main(int argc, const char* argv[]) {
     initVM();
 
-    Chunk chunk;
-    initChunk(&chunk);
-
-    // 4 - 3 * -2 without OP_SUBTRACT
-    writeConstant(&chunk, 4, 1);
-    writeConstant(&chunk, 3, 1);
-    writeConstant(&chunk, 2, 1);
-    writeChunk(&chunk, OP_NEGATE, 1);
-    writeChunk(&chunk, OP_MULTIPLY, 1);
-    writeChunk(&chunk, OP_NEGATE, 1);
-    writeChunk(&chunk, OP_ADD, 1);
-
-    writeChunk(&chunk, OP_RETURN, 2);
-
-    disassembleChunk(&chunk, "test chunk");
-    interpret(&chunk);
+    if (argc == 1) { // no file provided
+        repl();
+    }
+    else if (argc == 2) { // file provided
+        runFile(argv[1]);
+    }
+    else {
+        fprintf(stderr, "Usage: clox [path]\n");
+    }
 
     freeVM();
-    freeChunk(&chunk);
-    DESTROY_MEMORY();
     return 0;
 }
--- clox/src/vm.c
+++ clox/src/vm.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "common.h"
+#include "compiler.h"
 #include "debug.h"
 #include "vm.h"
 
@@ -118,8 +119,7 @@ Value* stackPeek = peek(); \
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk* chunk) {
-    vm.chunk = chunk;
-    vm.ip = vm.chunk->code;
-    return run();
+InterpretResult interpret(const char* source) {
+    compile(source);
+    return INTERPRET_OK;
 }
